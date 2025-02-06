72f12de120ecd58d9f95072979e0b2dac585a63f
mbiboss <122667057+mbiboss@users.noreply.github.com>
2025-02-06 22:49:22 +0600

    config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 
 module.exports = {
-  TOKEN: "",
+  TOKEN: "MTMzMjE5MjAyMjI5NTA4NTA4Ng.GAe_Lh.eMFricmt2UlHbPJo5HjYq7WB38aLbO8KF5yCak",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  ownerID: ["731544517542674443", ""], 
+  mongodbUri : "mongodb+srv://mbiboss:mbiboss@cluster0.lktx8.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
